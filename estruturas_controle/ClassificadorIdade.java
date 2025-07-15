281378b7149f4cd98ed131e97f3fc28a408c718c
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-15 17:54:02 -0300

    Adiciona código ClassificadorIdade e arquivos do Eclipse

---
+++ estruturas_controle/ClassificadorIdade.java
@@ -0,0 +1,17 @@
+package estruturas_controle;
+
+import java.util.Scanner;
+
+public class ClassificadorIdade {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int idade = sc.nextInt();
+        
+        System.out.println(idade);
+        
+        sc.close();
+
+    }
+}
