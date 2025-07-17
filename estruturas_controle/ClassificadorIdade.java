0a01d86718236aa6fa98afb4fdc257e330a4e25a
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-17 16:46:57 -0300

    Deleta a linha de coódigo de saída da variavel idade

--- estruturas_controle/ClassificadorIdade.java
+++ estruturas_controle/ClassificadorIdade.java
@@ -7,11 +7,11 @@ public class ClassificadorIdade {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int idade = sc.nextInt();
+        
         
         System.out.println("Testando comandos de git!");
         System.out.println("Hello World! Testando novos comandos de git! ");
-        System.out.println(idade);
+        
         
         sc.close();
 
