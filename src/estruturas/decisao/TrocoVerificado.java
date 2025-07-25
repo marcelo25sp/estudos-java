24c7fcf795984186aa545b0bad9ead46393119e6
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 01:45:14 -0300

    cria estrutura básica do programa com Scanner

---
+++ src/estruturas/decisao/TrocoVerificado.java
@@ -0,0 +1,19 @@
+package estruturas.decisao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TrocoVerificado {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Iniciando o programa Troco Verificado...");
+
+		sc.close();
+
+	}
+
+}
