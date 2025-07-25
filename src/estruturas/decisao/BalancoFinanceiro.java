8963c1c8e489da86b88efbc22a0eccf64ef36779
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 15:46:48 -0300

    implementa lógica condicional para analisar o saldo e informar se foi positivo, negativo ou zerado

--- src/estruturas/decisao/BalancoFinanceiro.java
+++ src/estruturas/decisao/BalancoFinanceiro.java
@@ -6,21 +6,30 @@ import java.util.Scanner;
 public class BalancoFinanceiro {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Balanço Financeiro");
 		System.out.println("------------------");
-		
+
 		System.out.print("Entre com o valor ganho no mês(em R$): ");
 		double receita = sc.nextDouble();
 		System.out.print("Entre com o valor total de despesas no mês(em R$): ");
 		double despesas = sc.nextDouble();
-		
+
+		System.out.println();
 		double saldo = receita - despesas;
 		System.out.printf("Saldo final: R$ %.2f%n", saldo);
-		
+
+		if (saldo > 0) {
+			System.out.println("Parabens, voce fechou o mês com saldo positivo!");
+		} else if (saldo < 0) {
+			System.out.println("Atenção! Você fechou o mes no saldo negativo!");
+		} else {
+			System.out.println("Seu saldo ficou zerado!");
+		}
+
 		sc.close();
 
 	}
