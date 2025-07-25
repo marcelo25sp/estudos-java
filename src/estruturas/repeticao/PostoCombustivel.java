b0a7f6a29c9fbe1504785ccdcc23ec2ca8157ff1
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 17:52:04 -0300

    cria estrutura inicial do programa

---
+++ src/estruturas/repeticao/PostoCombustivel.java
@@ -0,0 +1,20 @@
+package estruturas.repeticao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class PostoCombustivel {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Programa Posto de Combustível...");
+		System.out.println("--------------------------------");
+
+		sc.close();
+
+	}
+
+}
