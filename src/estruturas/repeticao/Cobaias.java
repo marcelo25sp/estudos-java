1c73afe39d531a7cdafa42415a0fbe3972ede6d3
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 20:43:08 -0300

    adiciona as variáveis que serão utilizadas no programa

--- src/estruturas/repeticao/Cobaias.java
+++ src/estruturas/repeticao/Cobaias.java
@@ -13,6 +13,15 @@ public class Cobaias {
 		System.out.println("Iniciando programa Cobaias...");
 		System.out.println("-----------------------------");
 
+		System.out.print("Quantos casos de testes serão digitados? ");
+		int n = sc.nextInt();
+
+		int cobaias = 0;
+		char tipoCobaias;
+		int coelhos = 0;
+		int ratos = 0;
+		int sapos = 0;
+
 		sc.close();
 
 	}
