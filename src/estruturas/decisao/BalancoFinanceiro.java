105d619e43578c716ac64f066ae465d5997b31b1
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 15:56:10 -0300

    Revisa, testa e valida o programa BalancoFinanceiro.java

--- src/estruturas/decisao/BalancoFinanceiro.java
+++ src/estruturas/decisao/BalancoFinanceiro.java
@@ -19,12 +19,13 @@ public class BalancoFinanceiro {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
+		Locale.setDefault(Locale.US); // Define o padrão de formatação numérica para o formato dos EUA (ex: ponto em vez de vírgula) 
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Balanço Financeiro");
 		System.out.println("------------------");
 
+		//entrada de dados do usuário
 		System.out.print("Entre com o valor ganho no mês(em R$): ");
 		double receita = sc.nextDouble();
 		System.out.print("Entre com o valor total de despesas no mês(em R$): ");
@@ -34,6 +35,7 @@ public class BalancoFinanceiro {
 		double saldo = receita - despesas;
 		System.out.printf("Saldo final: R$ %.2f%n", saldo);
 
+		// estrutura lógica que imprime a mensagem ao usuário
 		if (saldo > 0) {
 			System.out.println("Parabens, voce fechou o mês com saldo positivo!");
 		} else if (saldo < 0) {
