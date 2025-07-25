ec6f00741024dcc2bae9ba58e426fd46b75474bf
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 15:36:05 -0300

    adiciona entrada de dados (receita e despesas)

--- src/estruturas/decisao/BalancoFinanceiro.java
+++ src/estruturas/decisao/BalancoFinanceiro.java
@@ -13,6 +13,11 @@ public class BalancoFinanceiro {
 		System.out.println("Balanço Financeiro");
 		System.out.println("------------------");
 		
+		System.out.print("Entre com o valor ganho no mês(em R$): ");
+		double receita = sc.nextDouble();
+		System.out.print("Entre com o valor total de despesas no mês(em R$): ");
+		double despesas = sc.nextDouble();
+		
 		sc.close();
 
 	}
