d14e391222ba4386b645ec81c4021f20d3a03257
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 01:24:31 -0300

    implementa leitura das notas com ponto decimal

--- src/estruturas/decisao/Notas.java
+++ src/estruturas/decisao/Notas.java
@@ -1,12 +1,20 @@
 package estruturas.decisao;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Notas {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite a primeira nota do usuário: ");
+		double nota1 = sc.nextDouble();
+
+		System.out.print("Digite a segunda nota do usuário: ");
+		double nota2 = sc.nextDouble();
 
 		sc.close();
 
