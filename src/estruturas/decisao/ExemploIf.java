c8f89be71e5999ff6eca8d4ca311c2e6c1505c98
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 00:27:23 -0300

    Primeiro commit: estrutura do projeto e exemplo com if

---
+++ src/estruturas/decisao/ExemploIf.java
@@ -0,0 +1,17 @@
+package estruturas.decisao;
+
+public class ExemploIf {
+
+	public static void main(String[] args) {
+		
+		int idade = 18;
+
+		if (idade >= 18) {
+		    System.out.println("Você é maior de idade.");
+		} else {
+		    System.out.println("Você é menor de idade.");
+		}
+
+	}
+
+}
