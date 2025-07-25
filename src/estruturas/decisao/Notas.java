a007cc8469acc245413b6ae420843ce576689652
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 01:18:27 -0300

    cria estrutura básica do programa com Scanner

---
+++ src/estruturas/decisao/Notas.java
@@ -0,0 +1,15 @@
+package estruturas.decisao;
+
+import java.util.Scanner;
+
+public class Notas {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		sc.close();
+
+	}
+
+}
