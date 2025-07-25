ce86bdbd447bfd6ec64b6a798eac55a76794257a
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 18:11:01 -0300

    adiciona mensagem de agradecimento e imprime o número total de cada combustível escolhido

--- src/estruturas/repeticao/PostoCombustivel.java
+++ src/estruturas/repeticao/PostoCombustivel.java
@@ -12,24 +12,30 @@ public class PostoCombustivel {
 
 		System.out.println("Programa Posto de Combustível...");
 		System.out.println("--------------------------------");
-		
-		
+
 		int alcool = 0;
 		int gasolina = 0;
-		int diesel = 0;		
+		int diesel = 0;
 		int codigo = 0;
-		
-		while(codigo != 4) {
+
+		while (codigo != 4) {
 			System.out.print("Informe um código(1, 2, 3) ou 4 para parar: ");
 			codigo = sc.nextInt();
-			if(codigo == 1) {
+			if (codigo == 1) {
 				alcool++;
-			}else if(codigo == 2) {
+			} else if (codigo == 2) {
 				gasolina++;
-			}else if(codigo == 3) {
+			} else if (codigo == 3) {
 				diesel++;
 			}
 		}
+
+		System.out.println();
+		System.out.println("Muito obrigado!");
+		System.out.println("---------------");
+		System.out.println("Álcool: " + alcool);
+		System.out.println("Gasolina: " + gasolina);
+		System.out.println("Diesel: " + diesel);
 
 		sc.close();
 
