08c9b3593a21762452a1a90b52eb67883d447029
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 01:52:42 -0300

    implementa a leitura dos dados: preço, quantidade e dinheiro recebido

--- src/estruturas/decisao/TrocoVerificado.java
+++ src/estruturas/decisao/TrocoVerificado.java
@@ -4,12 +4,15 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class TrocoVerificado {
-	
-	/* Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia.
-O programa deve ler o preço unitário do produto, a quantidade de unidades compradas deste produto,
-e o valor em dinheiro dado pelo cliente. Seu programa deve mostrar o valor do troco a ser devolvido
-ao cliente. Se o dinheiro dado pelo cliente não for suficiente, mostrar uma mensagem informando o
-valor restante */
+
+	/*
+	 * Fazer um programa para calcular o troco no processo de pagamento de um
+	 * produto de uma mercearia. O programa deve ler o preço unitário do produto, a
+	 * quantidade de unidades compradas deste produto, e o valor em dinheiro dado
+	 * pelo cliente. Seu programa deve mostrar o valor do troco a ser devolvido ao
+	 * cliente. Se o dinheiro dado pelo cliente não for suficiente, mostrar uma
+	 * mensagem informando o valor restante
+	 */
 
 	public static void main(String[] args) {
 
@@ -17,6 +20,14 @@ valor restante */
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Iniciando o programa Troco Verificado...");
+
+		System.out.println();
+		System.out.print("Preço unitário do produto: ");
+		double preco = sc.nextDouble();
+		System.out.print("Quantidade comprada: ");
+		int quantidade = sc.nextInt();
+		System.out.print("Dinheiro recebido: ");
+		double valorRecebido = sc.nextDouble();
 
 		sc.close();
 
