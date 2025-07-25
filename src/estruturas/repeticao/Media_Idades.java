ff3a81f3e62b82da97644e2ed76b3ae8ebb64593
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 16:46:40 -0300

    cria estrutura inicial do programa com o Scanner

---
+++ src/estruturas/repeticao/Media_Idades.java
@@ -0,0 +1,20 @@
+package estruturas.repeticao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Media_Idades {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Cálculo da média das idades");
+		System.out.println("---------------------------");
+
+		sc.close();
+
+	}
+
+}
