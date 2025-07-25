04cba934ac8b1c2ed336bb914ce22ba4edcca4ba
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 01:34:31 -0300

    adiciona o enunciado do exercício

--- src/estruturas/decisao/Notas.java
+++ src/estruturas/decisao/Notas.java
@@ -4,6 +4,11 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Notas {
+	
+	/* Fazer um programa para ler as duas notas que um aluno obteve no primeiro e segundo semestres de
+	uma disciplina anual. Em seguida, mostrar a nota final que o aluno obteve (com uma casa decimal) no
+	ano juntamente com um texto explicativo. Caso a nota final do aluno seja inferior a 60.00, mostrar a
+	mensagem "REPROVADO" */
 
 	public static void main(String[] args) {
 
