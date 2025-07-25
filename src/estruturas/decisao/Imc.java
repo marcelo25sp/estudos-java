c0df9d05dee35d5ca0e79f57298e3601ec8f80cf
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 14:40:40 -0300

    cria estrutura inicial do programa com a classe Scanner

---
+++ src/estruturas/decisao/Imc.java
@@ -0,0 +1,20 @@
+package estruturas.decisao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Imc {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Cálculadora de IMC");
+		System.out.println("-------------------");
+
+		sc.close();
+
+	}
+
+}
