00e258968ba4c3fd175158704fde1f48455346bc
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 18:07:06 -0300

    implementa uma estrutura de decisão para fazer a contagem da opção alcool, gasolina ou diesel

--- src/estruturas/repeticao/PostoCombustivel.java
+++ src/estruturas/repeticao/PostoCombustivel.java
@@ -13,10 +13,22 @@ public class PostoCombustivel {
 		System.out.println("Programa Posto de Combustível...");
 		System.out.println("--------------------------------");
 		
+		
+		int alcool = 0;
+		int gasolina = 0;
+		int diesel = 0;		
 		int codigo = 0;
+		
 		while(codigo != 4) {
 			System.out.print("Informe um código(1, 2, 3) ou 4 para parar: ");
 			codigo = sc.nextInt();
+			if(codigo == 1) {
+				alcool++;
+			}else if(codigo == 2) {
+				gasolina++;
+			}else if(codigo == 3) {
+				diesel++;
+			}
 		}
 
 		sc.close();
