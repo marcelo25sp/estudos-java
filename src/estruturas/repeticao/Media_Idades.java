96fa06898eeda7ba9b5e235c5744c759a03bdabe
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 17:10:25 -0300

    adiciona o enunciado do exercício

--- src/estruturas/repeticao/Media_Idades.java
+++ src/estruturas/repeticao/Media_Idades.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 
 public class Media_Idades {
 
+	/*
+	 * Faça um programa para ler um número indeterminado de dados, contendo cada um,
+	 * a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém
+	 * um valor de idade negativa. Calcular e imprimir a idade média deste grupo de
+	 * indivíduos. Se for entrado um valor negativo na primeira vez, mostrar a
+	 * mensagem "Impossível calcular."
+	 */
+
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
