c4996a1671dddb73ae8ded9322e1265d8915e9e4
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 23:52:28 -0300

    cria a estrutura de repetição para a entrada de dados dos atletas

--- src/estruturas/repeticao/Atletas.java
+++ src/estruturas/repeticao/Atletas.java
@@ -6,13 +6,27 @@ import java.util.Scanner;
 public class Atletas {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Programa Atletas...");
 		System.out.println("-------------------");
-		
+
+		System.out.print("Qual a quantidade de atletas? ");
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			sc.nextLine(); //limpando o buffer
+			System.out.println("Digite os dados do atleta número " + (i + 1) + ":");
+			System.out.print("Nome: ");
+			String nome = sc.nextLine();
+			System.out.print("Sexo: ");
+			char genero = sc.next().charAt(0);
+			System.out.print("Altura: ");
+			double altura = sc.nextDouble();
+		}
+
 		sc.close();
 
 	}
