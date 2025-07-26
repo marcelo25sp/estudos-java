1b71f3689d7f9aa64fc567d71c9ea608cf16e618
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-26 20:13:54 -0300

    adiciona comentarios ao programa

--- src/estruturas/repeticao/Atletas.java
+++ src/estruturas/repeticao/Atletas.java
@@ -1,13 +1,13 @@
 package estruturas.repeticao;
 
-import java.util.Locale;
+import java.util.Locale; 
 import java.util.Scanner;
 
 public class Atletas {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
+		Locale.setDefault(Locale.US); // define o ponto como separador decimal
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Programa Atletas...");
@@ -15,7 +15,8 @@ public class Atletas {
 
 		System.out.print("Qual a quantidade de atletas? ");
 		int n = sc.nextInt();
-
+		
+		// variáveis para calculos e verificadas
 		double somaPeso = 0;
 		double maiorAltura = 0.0;
 		String maiorAtleta = null;
@@ -23,13 +24,18 @@ public class Atletas {
 		int contadorMulheres = 0;
 		double somaAltura = 0.0;
 
+		// loop principal para ler dados de cada atleta
 		for (int i = 0; i < n; i++) {
-			sc.nextLine(); // limpando o buffer
+			sc.nextLine(); // limpando o buffer antes de ler o nome,(evita pular linha)
+			
 			System.out.println("Digite os dados do atleta número " + (i + 1) + ":");
+			
 			System.out.print("Nome: ");
 			String nome = sc.nextLine();
 			System.out.print("Sexo: ");
 			char genero = sc.next().charAt(0);
+			
+			// validação do genero (garante que será digitado 'M' ou 'F'
 			while(genero != 'M' && genero != 'F') {
 				System.out.print("Valor invalido! Favor digitar F ou M: ");
 				genero = sc.next().charAt(0);
@@ -37,20 +43,28 @@ public class Atletas {
 			if (genero == 'M') {
 				contadorHomens++;
 			}
+			
+			// validação da altura (garante que será digitado um valor positivo)
 			System.out.print("Altura: ");
 			double altura = sc.nextDouble();
 			while(altura <= 0.0) {
 				System.out.print("Valor inválido! Digite um valor positivo: ");
 				altura = sc.nextDouble();
 			}
+			
+			//verifica se é a maior altura registrada e armazena na variável
 			if (altura > maiorAltura) {
 				maiorAltura = altura;
 				maiorAtleta = nome;
 			}
+			
+			// conta e soma a altura das mulheres para o cálculo posterior da média
 			if (genero == 'F') {
 				contadorMulheres++;
 				somaAltura += altura;
 			}
+			
+			// validação do peso (garante que será digitado um valor positivo)
 			System.out.print("Peso: ");
 			double peso = sc.nextDouble();
 			while(peso <= 0.0) {
@@ -60,6 +74,7 @@ public class Atletas {
 			somaPeso += peso;
 		}
 
+		// imprime o relatório final
 		System.out.println();
 		System.out.println("RELATÓRIO:");
 		System.out.println("----------");
