cafcf7fa5244ceb68900f07cf2ed2f16a0f7905e
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 23:21:45 -0300

    adiciona estrutura inicial do programa

---
+++ src/estruturas/repeticao/Tabuada.java
@@ -0,0 +1,20 @@
+package estruturas.repeticao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Programa Tabuada...");
+		System.out.println("--------------------");
+		
+		sc.close();
+
+	}
+
+}
