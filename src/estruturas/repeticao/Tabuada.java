358b3134316fee6fa1a7661afe6be463e69cdc6a
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 23:27:24 -0300

    implementa o loop para gerar a tabuada

--- src/estruturas/repeticao/Tabuada.java
+++ src/estruturas/repeticao/Tabuada.java
@@ -6,13 +6,20 @@ import java.util.Scanner;
 public class Tabuada {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Programa Tabuada...");
 		System.out.println("--------------------");
-		
+
+		System.out.print("Deseja calcular qual valor da tabuada? ");
+		int n = sc.nextInt();
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(n + " x " + i + " = " + (i * n));
+		}
+
 		sc.close();
 
 	}
