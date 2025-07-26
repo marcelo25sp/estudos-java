02c173a078fea54fcf9918b604194dc130e9279e
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-25 23:33:02 -0300

    adiciona comentários ao programa

--- src/estruturas/repeticao/Tabuada.java
+++ src/estruturas/repeticao/Tabuada.java
@@ -12,7 +12,7 @@ public class Tabuada {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
+		Locale.setDefault(Locale.US); // // Define o padrão de ponto flutuante como US (para usar '.' ao invés de ',')
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Programa Tabuada...");
@@ -21,6 +21,7 @@ public class Tabuada {
 		System.out.print("Deseja calcular qual valor da tabuada? ");
 		int n = sc.nextInt();
 
+		// loop para o cálculo da tabuada 
 		for (int i = 0; i <= 10; i++) {
 			System.out.println(n + " x " + i + " = " + (i * n));
 		}
