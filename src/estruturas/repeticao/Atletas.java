1d58dbfbaca37cee31934733be01d9aeacf61047
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-26 00:19:38 -0300

    cria uma estrutura de decisão para o cálculo da porcentagem de homens e o imprime

--- src/estruturas/repeticao/Atletas.java
+++ src/estruturas/repeticao/Atletas.java
@@ -15,33 +15,40 @@ public class Atletas {
 
 		System.out.print("Qual a quantidade de atletas? ");
 		int n = sc.nextInt();
-		
+
 		double somaPeso = 0;
 		double maiorAltura = 0.0;
 		String maiorAtleta = null;
-		
+		int contadorHomens = 0;
+
 		for (int i = 0; i < n; i++) {
-			sc.nextLine(); //limpando o buffer
+			sc.nextLine(); // limpando o buffer
 			System.out.println("Digite os dados do atleta número " + (i + 1) + ":");
 			System.out.print("Nome: ");
 			String nome = sc.nextLine();
 			System.out.print("Sexo: ");
 			char genero = sc.next().charAt(0);
+			if (genero == 'M') {
+				contadorHomens++;
+			}
 			System.out.print("Altura: ");
 			double altura = sc.nextDouble();
-			if(altura > maiorAltura) {
+			if (altura > maiorAltura) {
 				maiorAltura = altura;
 				maiorAtleta = nome;
 			}
 			System.out.print("Peso: ");
 			double peso = sc.nextDouble();
-			somaPeso += peso;			
+			somaPeso += peso;
 		}
-		
-		double mediaPeso = somaPeso/n;
+
+		double mediaPeso = somaPeso / n;
 		System.out.printf("Peso médio dos atletas = %.2f kg%n", mediaPeso);
-		
+
 		System.out.println("Maior atleta: " + maiorAtleta);
+
+		double percentualHomens = (double) contadorHomens * 100 / n;
+		System.out.printf("Percentual de homens: %.2f%%%n", percentualHomens);
 
 		sc.close();
 
